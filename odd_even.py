d2c0ea0cbc65013d6dd915be4c83bc1120547a11
shanthani <46441058+shanthani@users.noreply.github.com>
2019-01-07 15:26:51 +0530

    Update odd_even.py

---
+++ odd_even.py
@@ -0,0 +1,9 @@
+#shan
+n=int(input(""))
+if n>0:
+	if n%2==0:
+		print("Even")
+	else:
+		print("Odd")
+else:
+	print("invalid")
--- positive_negative.py
+++
@@ -1,8 +0,0 @@
-#shan
-n=int(input())
-if n>0:
-	print("Positive")
-elif n<0:
-	print("Negative")
-else:
-	print("Zero")
