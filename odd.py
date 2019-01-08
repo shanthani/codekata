e8ca02c253ce837b7b02e1f494ee38a60984ccc7
shanthani <46441058+shanthani@users.noreply.github.com>
2019-01-08 16:50:18 +0530

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,4 +1,10 @@
 n,k=map(int,input().split())
+c=0
 for i in range(n+1,k):
     if i%2==1:
-        print(i,end=" ")
+        if c==0:
+            print(i,end="")
+            c=c+1
+        else:
+            print(" ",i,end="")
+    
