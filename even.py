4e93577c8d4c243da03625f7c03c5f0b0188ad13
shanthani <46441058+shanthani@users.noreply.github.com>
2019-01-08 16:55:49 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,10 @@
+n,k=map(int,input().split())
+c=0
+for i in range(n+1,k):
+    if i%2==0:
+        if c==0:
+            print(i,end="")
+            c=c+1
+        else:
+            print("",i,end="")
+    
