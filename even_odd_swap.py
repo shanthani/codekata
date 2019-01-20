3190f64f22434e780c965195e4a379cfd8c781f9
shanthani <46441058+shanthani@users.noreply.github.com>
2019-01-20 10:57:43 +0530

    Update even_odd_swap.py

--- even_odd_swap.py
+++ even_odd_swap.py
@@ -1,7 +1,7 @@
 m=input()
 n=[x for x in m] 
-print(n)
+s=""
 for i in range(0,len(n),2): 
-    s="" n[i],n[i+1]=n[i+1],n[i] 
+    n[i],n[i+1]=n[i+1],n[i] 
     s=s+n[i]+n[i+1] 
-    print(s)
+print(s)
